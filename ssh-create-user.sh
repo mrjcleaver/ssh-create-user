ed052f6b1cba07373dda1d20991521707273915b
Martin Cleaver <Martin@Cleaver.org>
2016-06-02 18:09:03 -0400

    Update ssh-create-user.sh

--- ssh-create-user.sh
+++ ssh-create-user.sh
@@ -6,11 +6,13 @@ USER=$2
 PUB=$2
 
 [ -z "$PUB" && PUB=~/.ssh/id_rsa.pub ]
+[ -z "$SERVER" && echo "SERVER?" && exit 1 ]
+[ -z "$USER" && echo "USER?" && exit 1 ]
 
-echo ""
+echo "SUPERUSER=$SUPERUSER SERVER=$SERVER USER=$USER PUB=$PUB"
 
 cat $PUB | \
-  ssh $USER@$SERVER \
+  ssh $SUPERUSER@$SERVER \
   useradd $USER &&
   "sudo mkdir ~$USER/.ssh; sudo tee -a ~$USER/.ssh/authorized_keys"
   
