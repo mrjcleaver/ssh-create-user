9cf02898ba62f391fea57aa427d05d9f328f744c
Martin Cleaver <Martin@Cleaver.org>
2016-06-02 18:05:09 -0400

    Update ssh-create-user.sh

--- ssh-create-user.sh
+++ ssh-create-user.sh
@@ -1,13 +1,16 @@
 #! /bin/sh
 
+SUPERUSER=root
 SERVER=$1
 USER=$2
 PUB=$2
 
 [ -z "$PUB" && PUB=~/.ssh/id_rsa.pub ]
 
+echo ""
+
 cat $PUB | \
-  ssh root@$SERVER \
+  ssh $USER@$SERVER \
   useradd $USER &&
   "sudo mkdir ~$USER/.ssh; sudo tee -a ~$USER/.ssh/authorized_keys"
   
